df3edfb73f009d6ee87aa88d97d787db7d83984e
Josue Trejo <jt@jt-2.local>
2026-04-12 20:16:51 -0500

    Extract necessary data from api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -126,20 +126,20 @@ const LogButton = ({ name, path }: LogButtonProps) => (
 
 // Dummy Data
 const meals: MealCardProps[] = [
-  // {
-  //   mealType: "BREAKFAST",
-  //   calories: 450,
-  //   time: "8:34 AM",
-  //   macros: { protein: 24, carbs: 58, fats: 12 },
-  //   tags: [{ label: "High fiber", variant: "success" }],
-  // },
-  // {
-  //   mealType: "LUNCH",
-  //   calories: 680,
-  //   time: "1:00 PM",
-  //   macros: { protein: 58, carbs: 64, fats: 24 },
-  //   tags: [{ label: "Moderate sodium", variant: "warning" }],
-  // },
+  {
+    mealType: "BREAKFAST",
+    calories: 450,
+    time: "8:34 AM",
+    macros: { protein: 24, carbs: 58, fats: 12 },
+    tags: [{ label: "High fiber", variant: "success" }],
+  },
+  {
+    mealType: "LUNCH",
+    calories: 680,
+    time: "1:00 PM",
+    macros: { protein: 58, carbs: 64, fats: 24 },
+    tags: [{ label: "Moderate sodium", variant: "warning" }],
+  },
 ];
 
 const Meals = () => (
--- app/product-result.tsx
+++ app/product-result.tsx
@@ -2,22 +2,23 @@ import { useLocalSearchParams } from "expo-router";
 import { Text } from "@react-navigation/elements";
 import { View } from "react-native";
 import { useState, useEffect } from "react";
-import getData from "./api/openFoodFacts";
+import getData, { ProductResult } from "./api/openFoodFacts";
 
-export default function ProductResult() {
+export default function Product() {
   const { code } = useLocalSearchParams();
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState<ProductResult | null>(null);
 
   useEffect(() => {
     async function load() {
       const data = await getData(code);
-      setProduct(data);
+      // Narrow to silence undefined error
+      if (data) {
+        setProduct(data);
+        processResult(data);
+      }
     }
-
     load();
   }, [code]);
-
-  useEffect(() => {});
 
   return (
     <View>
@@ -25,3 +26,14 @@ export default function ProductResult() {
     </View>
   );
 }
+
+const processResult = (data: ProductResult) => {
+  console.log(data.status_verbose);
+  console.log(data.product.brands);
+  console.log(data.product.product_name);
+  console.log(data.product.nova_group);
+  console.log(data.product.nutriscore_grade);
+  console.log(data.product.nutrient_levels);
+  console.log(data.product.serving_size);
+  console.log(data.product.nutriments);
+};
