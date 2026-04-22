9c0011ff6bf7c4c99ab96c33f9dda60a4b385e66
Josue Trejo <jt@jt-2.local>
2026-04-22 15:21:49 -0500

    Fix auth headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
     <ProfileProvider>
       {/* initialRouteName="(auth)" */}
       <StatusBar style="dark" />
-      <Stack>
+      <Stack initialRouteName="(auth)" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="barcode-scan" options={{ title: "Scan Barcode" }} />
         <Stack.Screen
